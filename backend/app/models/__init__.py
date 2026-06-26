580dfc598150f764e59443fda4ca191d4c011d97
an0Nym0us63 <sarakha_ludovic@yahoo.fr>
2026-06-26 17:40:53 +0200

    feat: add Filament + Spool to models init

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 from .setting import Setting
 from .printer import PrinterState
+from .filament import Filament, Spool
 
-__all__ = ["Setting", "PrinterState"]
+__all__ = ["Setting", "PrinterState", "Filament", "Spool"]
